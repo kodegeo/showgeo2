3088d17cba9cb554a9c07587024a73ed4e254b5a
geodevelopment <132581797+kodegeo@users.noreply.github.com>
2026-05-03 07:43:58 -0600

    Updtae main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,14 +14,13 @@ async function bootstrap() {
 
   const isProd = process.env.NODE_ENV === "production";
 
-  app.enableCors(
-    isProd
-      ? { origin: true, credentials: true }
-      : {
-          origin: process.env.FRONTEND_URL || "http://localhost:5173",
-          credentials: true,
-        }
-  );
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "https://showgeo.vercel.app",
+    ],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
